8db121595820c5421958f90af5c920a0f84920b2
emblem333 <122144556+emblem333@users.noreply.github.com>
2023-02-18 13:10:58 +0000

    [Add]task3ﾚｲｱｳﾄ調整

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'homes/about' => "homes#about", as: "about"
   devise_for :users
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :destroy]
+  resources :books, only: [:index, :show, :edit,]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
